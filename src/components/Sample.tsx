df062e2842c30c3444af0a64195cd61fe702eb7d
umum253 <fkwpostpost@gmail.com>
2023-04-27 16:35:39 +0900

    first commit

---
+++ src/components/Sample.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+// 型定義
+type Data = {
+  user_id: string;
+  user_name: string;
+}
+
+// Sampleコンポーネント定義
+const Sample: React.FC = () => {
+  const url = "http://localhost:3000/users"; // URL
+  const [datas, setDatas] = useState([]); // レスポンスデータ
+
+  // GET APIを実行する関数
+  const getUserData = () => {
+    axios.get(url)
+    .then((res)=> {
+      setDatas(res.data);
+    })
+    .catch((e) => {
+      console.error(e);
+    })
+  }
+
+  return (
+    <>
+      <button onClick={getUserData}>
+        GET API実行
+      </button>
+      <div>
+        {datas.map((data: Data) => (
+          <div key={data.user_id}>
+            {data.user_id} : {data.user_name}
+          </div>
+        ))}
+       </div>
+    </>
+  );
+}
+
+export default Sample;
